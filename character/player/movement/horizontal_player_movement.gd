9245a0c511b3df822b2fe69426dfd5227fa7d631
AnonymousChief04 <kutsa2004@gmail.com>
2026-09-16 21:32:41 -0400

    Created Skeleton
    Needs to be re-organized into player input then to hpm

--- character/player/movement/horizontal_player_movement.gd
+++ character/player/movement/horizontal_player_movement.gd
@@ -1,11 +1,30 @@
 extends Node
 
+@export var player: CharacterBody2D
+@export var playerInput: Node
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+# Goes from Player Input to HPM
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func Movement(delta: float, velocity: Vector2) -> Vector2:
+	var direction: float = playerInput.GetDirection()
+	if direction:
+		# Right
+		if direction > 0:
+			velocity.x += direction * (player.acceleration * delta)
+			print("First Vel: ", velocity.x, " | ", player.acceleration)
+			if velocity.x > player.maxVelocity:
+				velocity.x -= move_toward(velocity.x, velocity.x - player.maxVelocity, player.dampening)
+			print("2nd Vel: ", velocity.x)
+		# Left
+		elif direction < 0:
+			velocity.x += direction * (player.acceleration * delta)
+			if velocity.x < (player.maxVelocity * -1):
+				velocity.x -= move_toward(velocity.x, velocity.x - (player.maxVelocity * -1), player.dampening)
+		# No Input
+		else:
+			velocity.x += move_toward(velocity.x, 0, player.deceleration)
+
+	else:
+		velocity.x += move_toward(velocity.x, 0, player.deceleration)
+	return velocity
--- character/player/player.gd
+++ character/player/player.gd
@@ -1,25 +1,23 @@
 extends CharacterBody2D
+class_name Player
 
 
-@export var speed : float = 800.0
-@export var jump_velocity : float = -2500.0
-@export var gravity : float = 4000.0
+# @export var jump_velocity : float = -2500.0
+# @export var gravity : float = 4000.0
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+# @export_category("Scripts")
+# @export var horizontalMovement: Node
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_velocity
+# func _physics_process(delta: float) -> void:
+# 	# Add the gravity.
+# 	if not is_on_floor():
+# 		velocity.y += gravity * delta
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+# 	# Handle jump.
+# 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+# 		velocity.y = jump_velocity
 
-	move_and_slide()
+# 	# Get the input direction and handle the movement/deceleration.
+# 	# As good practice, you should replace UI actions with custom gameplay actions.
+
+# 	move_and_slide()
