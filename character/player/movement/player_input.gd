50d4c21eec6ed149dbef0866f836c3ca9d538f76
Serena <168299125+SerenaSJ@users.noreply.github.com>
2026-09-16 21:26:18 -0400

    Early release attempt
    Early release multiplier = 0.4 test

--- character/player/movement/player_input.gd
+++ character/player/movement/player_input.gd
@@ -2,6 +2,8 @@ extends Node
 class_name  PlayerInput
 @export var wants_to_jump: bool = false
 @export var vertical_player_movement: VerticalPlayerMovement
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,7 +12,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		vertical_player_movement.jump()
-		
+	
+	
+	if Input.is_action_just_released("ui_accept"):
+		vertical_player_movement.early_release()
