9db8931db0b2063f7740d63059c5ec367b450bd1
Alora <a.tan.powell@gmail.com>
2026-09-16 21:04:05 -0400

    start of control to vertical movement
    haven't done jump() yet

--- character/player/movement/player_input.gd
+++ character/player/movement/player_input.gd
@@ -1,6 +1,7 @@
 extends Node
-
-
+class_name  PlayerInput
+@export var wants_to_jump: bool = false
+@export var vertical_player_movement: VerticalPlayerMovement
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,3 +10,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		wants_to_jump = true
+		
--- character/player/movement/vertical_player_movement.gd
+++ character/player/movement/vertical_player_movement.gd
@@ -1,9 +1,9 @@
 extends Node
-
+class_name VerticalPlayerMovement
 @export var speed : float = 800.0
 @export var jump_velocity : float = -2500.0
 @export var gravity : float = 4000.0
-
+#@onready var input : PlayerInput = PlayerInput
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,7 +14,9 @@ func _physics_process(delta: float) -> void:
 		get_parent().velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and get_parent().is_on_floor():
 		get_parent().velocity.y = jump_velocity
 
 	get_parent().move_and_slide()
+	
+	
+	
