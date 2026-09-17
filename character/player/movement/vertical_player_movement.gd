7a7648a2be93a34ed6574f895f704e71d7b2bc39
Alora <a.tan.powell@gmail.com>
2026-09-16 20:39:02 -0400

    jump in vertical move

--- character/player/movement/vertical_player_movement.gd
+++ character/player/movement/vertical_player_movement.gd
@@ -1,11 +1,20 @@
 extends Node
 
+@export var speed : float = 800.0
+@export var jump_velocity : float = -2500.0
+@export var gravity : float = 4000.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not get_parent().is_on_floor():
+		get_parent().velocity.y += gravity * delta
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and get_parent().is_on_floor():
+		get_parent().velocity.y = jump_velocity
+
+	get_parent().move_and_slide()
