94f755f2bd03e2591dd57d19a6c9291cd8daa9a0
52525rr <negitave509@gmail.com>
2026-09-16 22:03:32 -0400

    implemented CSV parser
    specifically, a function that takes in a String and returns an Array[PackedStringArray]
    someone with the other relevant files and datatypes needs to tell me what to do with this

--- system/dialogue/dialogue_parser.gd
+++ system/dialogue/dialogue_parser.gd
@@ -5,11 +5,12 @@ class_name CSVParser
 static func parse_dialogue_test(thing: Translation) -> Array:
 	var file := FileAccess.open("res://system/dialogue/data/test_dialogue.csv", FileAccess.READ);
 	var csv := file.get_as_text()
-	parse_csv_internal(csv)
+	var parsed = parse_csv_internal(csv)
+	print(parsed)
 	
 	return []
 	
-static func parse_csv_internal(csv: String) -> void:
+static func parse_csv_internal(csv: String) -> Array[PackedStringArray]:
 	var i := 0
 	var length := len(csv)
 	
@@ -19,7 +20,6 @@ static func parse_csv_internal(csv: String) -> void:
 	
 	var in_string_quotes := false
 	
-	
 	while i <= length:
 		var char := csv[i] if i < length else "\n"
 		i += 1
@@ -28,9 +28,16 @@ static func parse_csv_internal(csv: String) -> void:
 			line_builder.push_back(string_builder)
 			string_builder = ""
 		elif char == '"':
-			if i+1 < length and csv[i+1] == '"':
+			if i < length and csv[i] == '"':
 				string_builder += '"'
 				i += 1
 			else:
 				in_string_quotes = not in_string_quotes
-	
+		elif char == '\n' and not in_string_quotes:
+			line_builder.push_back(string_builder)
+			final_array.push_back(line_builder)
+			line_builder = PackedStringArray()
+			string_builder = ""
+		else:
+			string_builder += char
+	return final_array
