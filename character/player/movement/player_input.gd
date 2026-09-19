4ae6aa6a118664c2ae6efb267662dc0793d24594
Alora <a.tan.powell@gmail.com>
2026-09-19 14:48:00 -0400

    Jump (#119)
    * jump in vertical move
    
    * start of control to vertical movement
    
    haven't done jump() yet
    
    * jump
    
    jump
    
    * Early release attempt
    
    Early release multiplier = 0.4 test
    
    * bugfix+better multiplier
    
    0.55 seems better
    
    * Jump revisions
    
    ---------
    
    Co-authored-by: Serena <168299125+SerenaSJ@users.noreply.github.com>
    Co-authored-by: KaratePikachu <KaidenKeyZ@gmail.com>
    Co-authored-by: KaratePikachu <80354180+KaratePikachu@users.noreply.github.com>

--- character/player/movement/player_input.gd
+++ character/player/movement/player_input.gd
@@ -1,4 +1,6 @@
 extends Node
+class_name  PlayerInput
+@export var vertical_player_movement: VerticalPlayerMovement
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +11,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _input(event: InputEvent) -> void:
+	
+	##Jump Inputs
+	if event.is_action_pressed("jump"):
+		vertical_player_movement.jump()
+	elif event.is_action_released("jump"):
+		vertical_player_movement.early_release()
