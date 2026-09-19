75bb7baa87c8a3ae122fae944e308d66efd5d703
pop-jeff <155867512+pop-jeff@users.noreply.github.com>
2026-09-19 15:52:59 -0400

    Created base interact system (#116)
    * Created base interact system
    
    Added interactable_component.gd
    Added player_interact.gd
    created a interactable Area2D preafab
    
    * basic main menu and file select
    
    started work on sub-issue:
    btns (start/back/quit)
    
    * Modify code structure
    
    * Fix broken references caused by merging
    
    * Someone put their work on the wrong branch...
    
    ---------
    
    Co-authored-by: Alejandro Lara Lima <alejandrolaralima@gmail.com>
    Co-authored-by: KaratePikachu <KaidenKeyZ@gmail.com>
    Co-authored-by: KaratePikachu <80354180+KaratePikachu@users.noreply.github.com>

--- character/player/movement/player_input.gd
+++ character/player/movement/player_input.gd
@@ -1,7 +1,8 @@
 extends Node
 class_name  PlayerInput
-@export var vertical_player_movement: VerticalPlayerMovement
 
+@export var vertical_player_movement: VerticalPlayerMovement
+@export var player_interact_region : PlayerInteractRegion
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,7 +13,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
 func _input(event: InputEvent) -> void:
 	
 	##Jump Inputs
@@ -20,3 +20,7 @@ func _input(event: InputEvent) -> void:
 		vertical_player_movement.jump()
 	elif event.is_action_released("jump"):
 		vertical_player_movement.early_release()
+	
+	##Interact Inputs
+	if event.is_action_pressed("interact"):
+		player_interact_region.check_for_interactable()
