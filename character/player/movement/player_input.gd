a037b262c667501085f388e0b2b789c95a0a28f1
KaratePikachu <KaidenKeyZ@gmail.com>
2026-09-19 15:34:36 -0400

    Fix broken references caused by merging

--- character/player/movement/player_input.gd
+++ character/player/movement/player_input.gd
@@ -1,7 +1,7 @@
 extends Node
 class_name  PlayerInput
-@export var vertical_player_movement: VerticalPlayerMovement
 
+@export var vertical_player_movement: VerticalPlayerMovement
 @export var player_interact_region : PlayerInteractRegion
 
 # Called when the node enters the scene tree for the first time.
@@ -20,6 +20,7 @@ func _input(event: InputEvent) -> void:
 		vertical_player_movement.jump()
 	elif event.is_action_released("jump"):
 		vertical_player_movement.early_release()
-  
-  if event.is_action_pressed("interact"):
+	
+	##Interact Inputs
+	if event.is_action_pressed("interact"):
 		player_interact_region.check_for_interactable()
