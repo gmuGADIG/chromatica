f4ede8a3da1e3ee3e68e82aaddec6bb75ab9ead1
KaratePikachu <KaidenKeyZ@gmail.com>
2026-09-22 17:28:37 -0400

    Moved the code to input component

--- character/player/movement/player_input.gd
+++ character/player/movement/player_input.gd
@@ -4,6 +4,12 @@ class_name  PlayerInput
 @export var vertical_player_movement: VerticalPlayerMovement
 @export var player_interact_region : PlayerInteractRegion
 
+@export_group("Jump Buffer")
+# Time (seconds) in that a player character may still jump after 
+# pressing the jump button.
+@export var jump_buffer_time: float = 0.1
+
+var jump_buffer = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,13 +18,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if vertical_player_movement.can_jump() && jump_buffer:
+		vertical_player_movement.jump()
+		jump_buffer = false
 
 func _input(event: InputEvent) -> void:
 	
 	##Jump Inputs
 	if event.is_action_pressed("jump"):
-		vertical_player_movement.jump()
+		var jumped : bool = vertical_player_movement.jump()
+		if not jumped:
+			jump_buffer = true
+			get_tree().create_timer(jump_buffer_time).timeout.connect(func() : if jump_buffer: jump_buffer = false)
 	elif event.is_action_released("jump"):
 		vertical_player_movement.early_release()
 	
--- character/player/movement/vertical_player_movement.gd
+++ character/player/movement/vertical_player_movement.gd
@@ -11,15 +11,10 @@ class_name VerticalPlayerMovement
 ##If the player lets go of jump while still ascending, their vertical velocity will be multiplied by this number
 ##to give them better control over the player's jump.
 @export_range(0.0,1.0) var early_jump_release_multiplier : float = 0.55
-# Time (seconds) in that a player character may still jump after 
-# pressing the jump button.
-@export var jump_buffer_time: float = 0.1
-
 
 var can_early_jump_release : bool = false
 #@onready var input : PlayerInput = PlayerInput
 
-var jump_buffer = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -30,32 +25,24 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not player.is_on_floor():
 		player.velocity.y += gravity * delta
-	# Jump if player is on floor & the buffer is active.
-	else:
-		if player.is_on_floor && jump_buffer:
-			jump()
-			jump_buffer = false
+	
 	
 	player.move_and_slide()
 
 
+func can_jump() -> bool:
+	return player.is_on_floor()
+
 # Handle jump.	
-func jump() -> void:
-	if player.is_on_floor():
-		player.velocity.y = jump_velocity
-		can_early_jump_release = true
-	else:
-		# Begin jump buffering if not on floor while pressed.
-		jump_buffer = true
-		get_tree().create_timer(jump_buffer_time).timeout.connect(_on_jump_buffer_time_timeout)
+func jump() -> bool:
+	if not can_jump():
+		return false
+	player.velocity.y = jump_velocity
+	can_early_jump_release = true
+	return true
 
 
 func early_release() -> void:
 	if can_early_jump_release and player.velocity.y < 0:
 		player.velocity.y *= early_jump_release_multiplier
 		can_early_jump_release = false
-
-# Reset jump buffer boolean if 0.1s has passed since press.
-
-func _on_jump_buffer_time_timeout()->void:
-	jump_buffer = false 
