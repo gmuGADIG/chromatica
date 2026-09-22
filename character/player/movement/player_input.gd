8dfa66550bb192e9bc6f0f456e918c0f9e30c543
acherrysodacan <antonio.m.agosto@gmail.com>
2026-09-22 14:51:10 -0400

    Implemented Jump Buffer through Vertical Player Movement.
    Due to how jump is handled, I elected to have the jump buffer be initialized upon a failed calling of jump() wherein the player is not on the floor, and writing all of the code within vertical_player_movement. If this causes issues, please inform me and I shall correct. However, as it stands I believe there should be no problems.

--- character/player/movement/player_input.gd
+++ character/player/movement/player_input.gd
@@ -4,6 +4,7 @@ class_name  PlayerInput
 @export var vertical_player_movement: VerticalPlayerMovement
 @export var player_interact_region : PlayerInteractRegion
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
--- character/player/movement/vertical_player_movement.gd
+++ character/player/movement/vertical_player_movement.gd
@@ -11,10 +11,15 @@ class_name VerticalPlayerMovement
 ##If the player lets go of jump while still ascending, their vertical velocity will be multiplied by this number
 ##to give them better control over the player's jump.
 @export_range(0.0,1.0) var early_jump_release_multiplier : float = 0.55
+# Time (seconds) in that a player character may still jump after 
+# pressing the jump button.
+@export var jump_buffer_timer: float = 0.1
+
 
 var can_early_jump_release : bool = false
 #@onready var input : PlayerInput = PlayerInput
 
+var jump_buffer = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,6 +30,11 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not player.is_on_floor():
 		player.velocity.y += gravity * delta
+	# Jump if player is on floor & the buffer is active.
+	else:
+		if player.is_on_floor && jump_buffer:
+			jump()
+			jump_buffer = false
 	
 	player.move_and_slide()
 
@@ -34,9 +44,18 @@ func jump() -> void:
 	if player.is_on_floor():
 		player.velocity.y = jump_velocity
 		can_early_jump_release = true
+	else:
+		# Begin jump buffering if not on floor while pressed.
+		jump_buffer = true
+		get_tree().create_timer(jump_buffer_timer).timeout.connect(_on_jump_buffer_timer_timeout)
 
 
 func early_release() -> void:
 	if can_early_jump_release and player.velocity.y < 0:
 		player.velocity.y *= early_jump_release_multiplier
 		can_early_jump_release = false
+
+# Reset jump buffer boolean if 0.1s has passed since press.
+
+func _on_jump_buffer_timer_timeout()->void:
+	jump_buffer = false 
