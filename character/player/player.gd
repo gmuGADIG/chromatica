2319f864659b80d169b3738d31d9b13d3a8b8c6d
Drew Reynolds <drewreynolds080@gmail.com>
2026-09-23 19:59:33 -0400

    Update player.gd

--- character/player/player.gd
+++ character/player/player.gd
@@ -10,10 +10,10 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
+	# Handle basic jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
-	# Coyote Jump
+	# Handle coyote jump
 	else: if Input.is_action_just_pressed("ui_accept") and velocity.y < 0:
 		velocity.y = jump_velocity
 
