db500621e8fd02fbdf9279200c9c4855875fc5a6
AnonymousChief04 <kutsa2004@gmail.com>
2026-09-23 19:43:48 -0400

    Completed Movement

--- character/player/movement/horizontal_player_movement.gd
+++ character/player/movement/horizontal_player_movement.gd
@@ -1,30 +1,37 @@
 extends Node
 
+## Player reference and Input Reference
 @export var player: CharacterBody2D
 @export var playerInput: Node
 
-# Goes from Player Input to HPM
+# Player Movmement variables
+@export_category("Movement")
+@export var acceleration : float # Rate player accelerates
+@export var deceleration : float #Rate player Decreasesƒ
+@export var maxVelocity : float # Max Player Speed
+@export var dampening : float # How fast player decreases when over max speed
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func Movement(delta: float, velocity: Vector2) -> Vector2:
+func _physics_process(delta: float):
 	var direction: float = playerInput.GetDirection()
+	var velocity : Vector2 = get_parent().velocity
 	if direction:
 		# Right
 		if direction > 0:
-			velocity.x += direction * (player.acceleration * delta)
-			print("First Vel: ", velocity.x, " | ", player.acceleration)
-			if velocity.x > player.maxVelocity:
-				velocity.x -= move_toward(velocity.x, velocity.x - player.maxVelocity, player.dampening)
-			print("2nd Vel: ", velocity.x)
+			velocity.x += direction * (acceleration * delta)
+			if velocity.x > maxVelocity:
+				print("Over accelerating: ", velocity)
+				velocity.x = move_toward(velocity.x, maxVelocity, delta * dampening)
+				print("After Correction: ", velocity.x)
 		# Left
 		elif direction < 0:
-			velocity.x += direction * (player.acceleration * delta)
-			if velocity.x < (player.maxVelocity * -1):
-				velocity.x -= move_toward(velocity.x, velocity.x - (player.maxVelocity * -1), player.dampening)
-		# No Input
-		else:
-			velocity.x += move_toward(velocity.x, 0, player.deceleration)
-
+			velocity.x += direction * (acceleration * delta)
+			if velocity.x < (maxVelocity * -1):
+				velocity.x = move_toward(velocity.x, maxVelocity * -1, delta * dampening)
+	# No Input
 	else:
-		velocity.x += move_toward(velocity.x, 0, player.deceleration)
-	return velocity
+			velocity.x = move_toward(velocity.x, 0, delta * deceleration)
+
+	#Done so the velocity is properly updated
+	get_parent().velocity = velocity
+	get_parent().move_and_slide()
