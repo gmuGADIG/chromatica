2ffd4b842b2b1cd4925ad5beeb1cb33f3b7c3354
KaratePikachu <KaidenKeyZ@gmail.com>
2026-09-24 15:14:24 -0400

    give default minimum of 3

--- character/player/player_health_component.gd
+++ character/player/player_health_component.gd
@@ -2,7 +2,7 @@ extends HealthComponent
 
 class_name PlayerHealthComponent
 
-static var maximum_hp_player: int
+static var maximum_hp_player: int = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
