189dc990a4ca57f33bec7c8ec0e64441220cd8f3
pop-jeff <klauerwade@gmail.com>
2026-09-23 20:18:04 -0400

    Commented colorable_component

--- world/colorable/colorable_component.gd
+++ world/colorable/colorable_component.gd
@@ -7,11 +7,14 @@ signal color_changed
 var colors : Array = []
 
 func apply(color : Color) -> void:
+	# if the colorable does not have 2 colors, add ther color
 	if(colors.size() > 2):
 		colors.append(color)
+	# if the colorable has 2 colors, move the second color back and replace the second color with the added color
 	else:
 		colors[0] = colors[1]
 		colors[1] = color
+	# whenever a color is applied the color has been changed
 	color_changed.emit()
 
 func varnish() -> void:
