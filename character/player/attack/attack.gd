4de753d4858aca8c48dafefac8628453cd66a3aa
AriCodesShit <heyimaveryanimates@gmail.com>
2026-09-23 20:02:27 -0400

    Attack
    tells you what needs to be added

--- character/player/attack/attack.gd
+++ character/player/attack/attack.gd
@@ -1,13 +1,16 @@
 extends Area2D
 
 @export var attack_duration: float = 0.2
+#change to a longer time when find out what durration should be
 
 func _input(event: InputEvent):
 	if event.is_action_pressed("attack"):
 		# Prevent attacking when already mid-attack
 		if monitoring==true: return
+		#maybe wepon move
 		monitoring = true
 		print("now true")
 		await get_tree().create_timer(attack_duration).timeout
 		monitoring = false
+		#maybe wepon move back to wherever it came from
 		print("now false")
