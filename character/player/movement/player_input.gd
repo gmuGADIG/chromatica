5501fbd79b4e9c046b30f1302ac260ae4a86158f
Camber Boles <camberb51@gmail.com>
2026-09-23 20:16:18 -0400

    anger and violence (kaiden told me to organize the code)

--- character/player/movement/player_input.gd
+++ character/player/movement/player_input.gd
@@ -1,11 +1,18 @@
 extends Node
 
+@export var coyote_time : float = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var player_root : Player = $"../Player" as Player
+@onready var coyote_timer : Timer = $InputComponent/CoyoteTimer as Timer
+var coyote_eligible : bool = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+func handle_jump() -> void:
+	# Handle basic jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		player_root.velocity.y = jump_velocity
+	# Handle coyote jump
+	elif Input.is_action_just_pressed("ui_accept") and velocity.y < 0 and coyote_time < 0.1:
+		player_root.velocity.y = jump_velocity
+	
