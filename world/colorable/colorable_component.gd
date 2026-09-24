47c0487e230a7c816f7eca2ce3d1a0fd32df5887
pop-jeff <klauerwade@gmail.com>
2026-09-23 20:53:47 -0400

    changed comments on colorable-component

--- world/colorable/colorable_component.gd
+++ world/colorable/colorable_component.gd
@@ -4,20 +4,20 @@ class_name Colorable
 @export var color_target : Node2D
 signal combo_triggered
 signal color_changed
-var colors : Array = []
+var colors : Array[Color] = []
 
 func apply(color : Color) -> void:
-	# if the colorable does not have 2 colors, add ther color
+	# if the colorable does not have 2 colors, add the color
 	if(colors.size() > 2):
 		colors.append(color)
-	# if the colorable has 2 colors, move the second color back and replace the second color with the added color
 	else:
+		# move second color to first, set second color as the applied color
 		colors[0] = colors[1]
 		colors[1] = color
 	# whenever a color is applied the color has been changed
 	color_changed.emit()
 
 func varnish() -> void:
-	if(colors.size() == 2):
+	if(colors.size() == 2 and colors[0] != colors[1]):
 		combo_triggered.emit()
 	colors = []
