4b06a44d1792c0747bbf84ea8184e0e915f1bfed
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2026-09-23 21:18:47 -0400

    start work on entity spawner

---
+++ ldtk/scripts/entity_spawner.gd
@@ -0,0 +1,9 @@
+extends Node
+class_name EntitySpawner
+
+@export var entity_layer : LDTKEntityLayer
+
+func _ready() -> void:
+	# access array of entities using 'entity_layer.entities'
+	# forward correct entity based on data
+	pass
