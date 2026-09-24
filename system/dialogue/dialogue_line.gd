38c52e54777efb8706b64061e1295fc64b55ec27
52525rr <negitave509@gmail.com>
2026-09-24 19:54:15 -0400

    variable renaming

--- system/dialogue/dialogue_line.gd
+++ system/dialogue/dialogue_line.gd
@@ -3,24 +3,24 @@ extends Resource
 class_name DialogueLine 
 
 ## Speaker ID
-var speaker_id: String
+var _speaker_id: String
 
 ## Speaker Text
-var dialogue_text: String
+var _dialogue_text: String
 
 ## Creates a new DialogueLine with a given speaker ID and text.
-func _init(_speaker_id: String, _dialogue_text: String) -> void:
-	speaker_id = _speaker_id
-	dialogue_text = _dialogue_text
+func _init(speaker_id: String, dialogue_text: String) -> void:
+	self._speaker_id = speaker_id
+	self._dialogue_text = dialogue_text
 
 ## Gets the text associated with this DialogueLine.
 func get_dialogue_text() -> String:
-	return dialogue_text
+	return _dialogue_text
 	
 ## Gets the speaker ID associated with this DialogueLine.
 func get_speaker_id() -> String:
-	return speaker_id
+	return _speaker_id
 	
 # for debugging purposes
 func _to_string() -> String:
-	return speaker_id + ": " + dialogue_text
+	return _speaker_id + ": " + _dialogue_text
