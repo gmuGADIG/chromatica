4282e89ba843123b740bc2f3695c01215169d5dc
Fleshtus <stinkybabymonkey@gmail.com>
2026-09-24 18:35:01 -0400

    Volume Sliders Added
    Done:
    -all 3 volume sliders are added, they all start at max volume
    -each volume slider adjusts the respective audio bus
    -there is a button that hides the SettingsMenu scene
    -there is a button in the PauseMenu scene to show the SettingsMenu scene

--- ui/pause_menu/pause_menu.gd
+++ ui/pause_menu/pause_menu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var SettingsMenu: Control = $SettingsMenu
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +9,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+
+func _on_settings_button_pressed() -> void:
+	SettingsMenu.show() #shows the settings menu
 	pass
--- ui/settings_menu/settings_menu.gd
+++ ui/settings_menu/settings_menu.gd
@@ -1,8 +1,11 @@
 extends Control
 
+var master_bus_index
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
+	
 	pass # Replace with function body.
 
 
@@ -11,5 +14,24 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_value_changed(value: float) -> void:
+func _on_button_pressed() -> void:
+	hide() # hides the settings menu
+	pass # Replace with function body.
+
+# when the Master volume slider is changed
+func _on_master_volume_changed(value: float) -> void:
+	# changes the audio bus to the volume slider value
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(value))
+	pass # Replace with function body.
+	
+# when the SFX volume slider is changed
+func _on_sfx_volume_changed(value: float) -> void:
+	# changes the audio bus to the volume slider value
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear_to_db(value))
+	pass # Replace with function body.
+
+# when the BGM volume slider is changed
+func _on_bgm_volume_changed(value: float) -> void:
+	# changes the audio bus to the volume slider value
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("BGM"), linear_to_db(value))
 	pass # Replace with function body.
