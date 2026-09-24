5157f7fa6fe69bb9b995cde1a2b679bb33464ca5
KaratePikachu <KaidenKeyZ@gmail.com>
2026-09-24 15:26:34 -0400

    Fixed bug
    was checking for size > 2 rather than size < 2 for adding a new color

--- world/colorable/colorable_component.gd
+++ world/colorable/colorable_component.gd
@@ -6,14 +6,14 @@ signal combo_triggered
 signal color_changed
 var colors : Array[Color] = []
 
-func apply(color : Color) -> void:
+func apply(new_color : Color) -> void:
+	assert(colors.size() <= 2, "Somehow has too many colors")
 	# if the colorable does not have 2 colors, add the color
-	if(colors.size() > 2):
-		colors.append(color)
-	else:
+	if(colors.size() < 2):
+		colors.append(new_color)
 		# move second color to first, set second color as the applied color
 		colors[0] = colors[1]
-		colors[1] = color
+		colors[1] = new_color
 	# whenever a color is applied the color has been changed
 	color_changed.emit()
 
