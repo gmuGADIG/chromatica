fbb000a567f0cfa52c9a9e98d7b4fa690b0a8824
Crafty <59181698+Crafty-The-Fox@users.noreply.github.com>
2026-09-23 21:28:15 -0400

    Update dialogue_starter.gd

--- system/dialogue/dialogue_starter.gd
+++ system/dialogue/dialogue_starter.gd
@@ -1,4 +1,13 @@
 extends Node
 class_name DialogueStarter
 
-@export var dialogue_csv : Translation
+@export_file_path("*.csv") var dialogue_path: String
+
+func _ready() -> void:
+	print("asdf")
+	start_dialogue("res://system/dialogue/data/test_dialogue.csv")
+
+static func start_dialogue(dialogue_path):
+	var dialogue := CSVParser.parse_dialogue(dialogue_path)
+	print(dialogue[3].get_speaker_id())
+	
